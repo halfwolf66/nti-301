5dc33c5aace9bca76f92a09e2bcc98a9acdb712b
Sfenner <55862990+Sfenner@users.noreply.github.com>
2019-10-10 18:48:44 -0700

    Update packages2.sh

--- packages2.sh
+++ packages2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-yum -y install wget
+if [ -e /usr/bin/wget ]; then
+  exit 0;
+fi
+yum -y install wget #installs wget
 wget https://raw.githubusercontent.com/Sfenner/nti-300/master/package.txt
 for packages in $(cat packages.txt); do
   yum -y install $packages
