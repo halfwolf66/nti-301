44dae637e4ee40c9c3fcf2754ddb01a8aca0a11a
Sfenner <55862990+Sfenner@users.noreply.github.com>
2019-11-07 18:13:46 -0800

    Create apacheautomation.sh

---
+++ apacheautomation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ -e /etc/httpd ]; then exit0; fi
+yum -y install httpd mod_ssl                                                                  #install apache and SSL support
+systemctl start httpd                                                                           #start apache server
+sed -i 's/^/#/g' welcome.conf                                                                 #comment out welcome page
+echo "<html><body><body><h1>Hi there NTI 300</h1></body></html>" > /var/www/html/index.html   #create custom welcome page
+systemctl restart httpd       
